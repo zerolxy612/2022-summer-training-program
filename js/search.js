22b641d2ab85320e3274f875869be4de39dd5c68
zerolxy <1056180778@qq.com>
2022-09-25 20:02:05 +0800

    '搜索框弹出实现'

--- js/search.js
+++ js/search.js
@@ -1,27 +1,43 @@
 'use strict'
-// 按钮选择
+
+const searchImg = document.querySelector('.list-top-img1')
+const listTop = document.querySelector('.list-top')
+const listMain = document.querySelector('.list-main')
+const searchMain = document.querySelector('.search-main')
 const dateBtn = document.querySelectorAll('.search-date-span')
 const channelBtn = document.querySelectorAll('.search-channel-btn')
 const searchBtn = document.querySelector('.search-btn')
-
-const btnChoose = function (btnType,activeClass) {
+//搜索框出现
+let direction = 'left'
+searchImg.addEventListener('click', function () {
+  searchMain.classList.add(`slide-${direction}`)
+  listMain.classList.add(`slide-${direction}-out`)
+  direction = direction === 'left' ? 'right' : 'left'
+  if(direction === 'right'){
+    searchMain.classList.remove(`slide-right`)
+    listMain.classList.remove(`slide-right-out`)
+  }
+  console.log(direction);
+})
+// 按钮选择
+const btnChoose = function (btnType, activeClass) {
   let cnt = 0
   btnType.forEach(function (btn) {
-  btn.addEventListener('click', function () {
-    if (btn.classList.contains(`${activeClass}`)) {
-      btn.classList.remove(`${activeClass}`)
-      cnt--
-    } else if (cnt < 1) {
-      btn.classList.add(`${activeClass}`)
-      cnt++
-    }
+    btn.addEventListener('click', function () {
+      if (btn.classList.contains(`${activeClass}`)) {
+        btn.classList.remove(`${activeClass}`)
+        cnt--
+      } else if (cnt < 1) {
+        btn.classList.add(`${activeClass}`)
+        cnt++
+      }
+    })
   })
-})
 }
 
-btnChoose(dateBtn,'search-date-active');
-btnChoose(channelBtn,'search-channel-active');
+btnChoose(dateBtn, 'search-date-active')
+btnChoose(channelBtn, 'search-channel-active')
 
-searchBtn.addEventListener('click',function(){
-  searchBtn.style.backgroundColor = '#D5EF7F';
+searchBtn.addEventListener('click', function () {
+  searchBtn.style.backgroundColor = '#D5EF7F'
 })
