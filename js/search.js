95cebd8bf5bc2b554dcc4316d3f6b36d2c180c9d
zerolxy <1056180778@qq.com>
2022-09-29 16:58:55 +0800

    'search.js错误修复'

--- js/search.js
+++ js/search.js
@@ -5,39 +5,53 @@ const listTop = document.querySelector('.list-top')
 const listMain = document.querySelector('.list-main')
 const searchMain = document.querySelector('.search-main')
 const dateBtn = document.querySelectorAll('.search-date-span')
+const laterBtn = document.querySelector('#later')
 const channelBtn = document.querySelectorAll('.search-channel-btn')
 const searchBtn = document.querySelector('.search-btn')
+console.log(searchImg);
 //搜索框出现
 let direction = 'left'
 searchImg.addEventListener('click', function () {
   searchMain.classList.add(`slide-${direction}`)
   listMain.classList.add(`slide-${direction}-out`)
   direction = direction === 'left' ? 'right' : 'left'
-  if(direction === 'right'){
+  if (direction === 'right') {
     searchMain.classList.remove(`slide-right`)
     listMain.classList.remove(`slide-right-out`)
   }
-  console.log(direction);
+  console.log(direction)
 })
+
 // 按钮选择
 const btnChoose = function (btnType, activeClass) {
-  let cnt = 0
+  let cnt = 0;
   btnType.forEach(function (btn) {
     btn.addEventListener('click', function () {
       if (btn.classList.contains(`${activeClass}`)) {
         btn.classList.remove(`${activeClass}`)
         cnt--
+        console.log(cnt);
       } else if (cnt < 1) {
         btn.classList.add(`${activeClass}`)
         cnt++
+        console.log(cnt);
+      }else if(cnt === 1){
+        btnType.forEach(function(btnAll){
+          btnAll.classList.remove(`${activeClass}`)
+        })
+        btn.classList.add(`${activeClass}`)
+        console.log(cnt);
       }
     })
   })
 }
+
+
 
 btnChoose(dateBtn, 'search-date-active')
 btnChoose(channelBtn, 'search-channel-active')
 
 searchBtn.addEventListener('click', function () {
   searchBtn.style.backgroundColor = '#D5EF7F'
+  location.replace('../HTML/no-result.html')
 })
